096065f2e44732c9bcdf346cd8a438c9a207b7ae
Vagan Manukian <vagan@Vagans-MacBook-Pro.local>
2020-04-01 02:27:10 +0300

    touchevents

--- script.js
+++ script.js
@@ -53,16 +53,49 @@ window.onload = function() {
             //name.style["font-variation-settings"] = "'wght' " + variable;
     }
 
+    var touchanimate = function(e) {
+        var touch = e.touches[0];
+        x = ((touch.clientX/window.innerWidth)*2-1)*45;
+        y = ((touch.clientY/window.innerHeight)*2-1)*45;
+        onetozero = Math.abs((touch.clientX/(window.innerWidth-border))-1);
+        margin = touch.clientX*margk;
+        margintop = Math.abs((touch.clientY/(window.innerHeight))-1)*margtopk;
+        scale = onetozero;
+        rotate = onetozero*45;
+        linkcolor = touch.clientX/(window.innerWidth-border);
+        wrap.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+        if (touch.clientX<window.innerWidth-border) {
+            for (i=0; i<projects.length; i++) {
+                projects[i].style.transform = 'scale(' + (scale/i + 1) + ')';
+            }
+            for (i=0; i<links.length; i++) {
+                links[i].style.transform = 'rotate(' + (rotate/i) + 'deg)';
+                links[i].style.marginTop =  margintop + 'px';
+                links[i].style.width = 'auto';
+            }
+            wrap.style.marginLeft = margin + 'px';
+        } else {
+            for (i=0; i<projects.length; i++) {
+                projects[i].style.transform = 'scale(' + 1 + ')';
+            }
+            for (i=0; i<links.length; i++) {
+                links[i].style.transform = 'rotate(' + 0 + 'deg)';
+                links[i].style.width = '100%';
+            }
+            wrap.style.marginLeft = (window.innerWidth-border)*margk + 'px';
+            console.log(wrap.innerWidth);
+        }
+}
+
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (isMobile || (window.innerWidth < 1080)){
-
+        window.addEventListener('touchmove',touchanimate);
     } else {
         window.addEventListener('mousemove',animate);
         document.addEventListener('mouseleave',function(){
             wrap.style.opacity = '0.1';
             wrap.style.transition = 'opacity 0.3s';
         });
-    
         document.addEventListener('mouseenter',function(){
             wrap.style.opacity = '1';
             wrap.style.transition = 'opacity 0.6s';
