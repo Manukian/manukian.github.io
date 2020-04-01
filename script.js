ad21d052aa9e95471d2fef512a33eeaa07bbf97e
Vagan Manukian <vagan@Vagans-MacBook-Pro.local>
2020-04-01 13:51:03 +0300

    touchevents4

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ window.onload = function() {
     wrap.style.minHeight = window.innerHeight-48+ 'px';
     var projects = document.getElementsByClassName('project-info');
     var links = document.getElementsByClassName('project-link');
-    var role = document.getElementById('role');
-    var name = document.getElementById('name');
 
     changecol = function(input,output){
         return ((input - output)*linkcolor + output);
@@ -21,11 +19,9 @@ window.onload = function() {
             margintop = Math.abs((e.clientY/(window.innerHeight))-1)*margtopk;
             scale = onetozero;
             rotate = onetozero*45;
-            //variable = onetozero*1000;
-            //letter = onetozero*45;
             linkcolor = e.clientX/(window.innerWidth-border);
             wrap.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
-            if (e.clientX<window.innerWidth-border) {
+            if (e.clientX < window.innerWidth-border) {
                 for (i=0; i<projects.length; i++) {
                     projects[i].style.transform = 'scale(' + (scale/i + 1) + ')';
                 }
@@ -35,8 +31,6 @@ window.onload = function() {
                     links[i].style.width = 'auto';
                 }
                 wrap.style.marginLeft = margin + 'px';
-                //role.style.letterSpacing = letter - 0.4 + 'px';
-                //name.style.color = 'rgb(' + changecol(1,198) + ', ' + changecol(153,1) + ', ' + changecol(68,131) + ')';
             } else {
                 for (i=0; i<projects.length; i++) {
                     projects[i].style.transform = 'scale(' + 1 + ')';
@@ -47,25 +41,24 @@ window.onload = function() {
                 }
                 wrap.style.marginLeft = (window.innerWidth-border)*margk + 'px';
                 console.log(wrap.innerWidth);
-                //role.style.letterSpacing = - 0.4 + 'px';
-                //name.style.color = 'rgb(1,153,68)';
             }
-            //name.style["font-variation-settings"] = "'wght' " + variable;
     }
+
+    var borderTouch = window.innerHeight*0.5;
+    var margkTouch = 0.55;
 
     var touchanimate = function(e) {
         e.preventDefault();
         var touch = e.touches[0];
         x = ((touch.clientX/window.innerWidth)*2-1)*45;
         y = ((touch.clientY/window.innerHeight)*2-1)*45;
-        onetozero = Math.abs((touch.clientX/(window.innerWidth-border))-1);
-        margin = touch.clientX*margk;
+        onetozero = Math.abs((touch.clientY/(window.innerHeight-borderTouch))-1);
         margintop = Math.abs((touch.clientY/(window.innerHeight))-1)*margtopk;
         scale = onetozero;
         rotate = onetozero*45;
-        linkcolor = touch.clientX/(window.innerWidth-border);
+        linkcolor = touch.clientY/(window.innerHeight-borderTouch);
         wrap.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
-        if (touch.clientX<window.innerWidth-border) {
+        if (touch.clientY < window.innerHeight-borderTouch) {
             for (i=0; i<projects.length; i++) {
                 projects[i].style.transform = 'scale(' + (scale/i + 1) + ')';
             }
@@ -74,7 +67,6 @@ window.onload = function() {
                 links[i].style.marginTop =  margintop + 'px';
                 links[i].style.width = 'auto';
             }
-            wrap.style.marginLeft = margin + 'px';
         } else {
             for (i=0; i<projects.length; i++) {
                 projects[i].style.transform = 'scale(' + 1 + ')';
@@ -83,14 +75,12 @@ window.onload = function() {
                 links[i].style.transform = 'rotate(' + 0 + 'deg)';
                 links[i].style.width = '100%';
             }
-            wrap.style.marginLeft = (window.innerWidth-border)*margk + 'px';
-            console.log(wrap.innerWidth);
         }
 }
 
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (isMobile || (window.innerWidth < 1080)){
-        //window.addEventListener('touchmove',touchanimate);
+        window.addEventListener('touchmove',touchanimate);
     } else {
         window.addEventListener('mousemove',animate);
         document.addEventListener('mouseleave',function(){
