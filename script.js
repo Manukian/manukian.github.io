4423b8af581f949b79bac21abac4eb8ddfe49235
Vagan Manukian <vagan@Vagans-MacBook-Pro.local>
2020-04-01 20:05:56 +0300

    touchevents12

--- script.js
+++ script.js
@@ -42,51 +42,52 @@ window.onload = function() {
             }
     }
 
-    var borderTouch = window.innerHeight*0.1;
-    var margkTouch = 0.4;
-    var topSide = 200;
- 
+    var borderTouch = window.innerWidth*0.25;
+    var margkTouch = 1;
+    var leftSide = 50;
+
     var touchanimate = function(e) {
         var touch = e.touches[0];
         x = ((touch.clientX/window.innerWidth)*2-1)*45;
         y = ((touch.clientY/window.innerHeight)*2-1)*45;
-        zerotoone = (touch.clientY-topSide)/(window.innerHeight-borderTouch-topSide);
-        margin = (touch.clientY-topSide)*margkTouch;
-        margintop = touch.clientX/(window.innerWidth)*margtopk;
+        zerotoone = (touch.clientX-leftSide)/(window.innerWidth-borderTouch-leftSide);
+        margin = (touch.clientX-leftSide)*margkTouch;
+        margintop = touch.clientY/(window.innerHeight)*margtopk;
         scale = zerotoone;
         rotate = zerotoone*45;
-        document.body.style.overflowy = 'hidden';
+        for (i=0; i<links.length; i++) {
+            links[i].style.marginTop =  margintop + 'px';
+        }
         //wrap.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
-        if (touch.clientY <= topSide) {
+        if (touch.clientX <= leftSide) {
             for (i=0; i<projects.length; i++) {
                 projects[i].style.transform = 'scale(' + 1 + ')';
             }
             for (i=0; i<links.length; i++) {
                 links[i].style.transform = 'rotate(' + 0 + 'deg)';
-                links[i].style.marginTop =  margintop + 'px';
                 links[i].style.width = '100%';
             }
             wrap.style.marginLeft = 0 + 'px';
         }
-        if ((topSide < touch.clientY) && (touch.clientY < window.innerHeight-borderTouch)) {
+        if ((leftSide < touch.clientX) && (touch.clientX < window.innerWidth-borderTouch)) {
             for (i=0; i<projects.length; i++) {
                 projects[i].style.transform = 'scale(' + (scale/i + 1) + ')';
             }
             for (i=0; i<links.length; i++) {
                 links[i].style.transform = 'rotate(' + (rotate/i) + 'deg)';
-                links[i].style.marginTop =  margintop + 'px';
                 links[i].style.width = 'auto';
             }
             wrap.style.marginLeft = margin + 'px';
         }
-        if (touch.clientY > window.innerHeight-borderTouch) {
-            wrap.style.marginLeft = (window.innerHeight-borderTouch-topSide)*margkTouch + 'px';
+        if (touch.clientX > window.innerWidth-borderTouch) {
+            wrap.style.marginLeft = (window.innerWidth-borderTouch-leftSide)*margkTouch + 'px';
         }
 }
 
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (isMobile || (window.innerWidth < 1080)){
         wrap.classList.add('break');
+        document.documentElement.style.overflowY = 'hidden';
         window.addEventListener('touchmove',touchanimate);
     } else {
         window.addEventListener('mousemove',animate);
