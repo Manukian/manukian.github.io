f284e9b19a3fb914335a516cb6264d3cc49b08cd
Vagan Manukian <vagan@Vagans-MacBook-Pro.local>
2020-04-02 03:23:09 +0300

    touchevents17

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ window.onload = function() {
     wrap.style.minHeight = window.innerHeight-48+ 'px';
     var projects = document.getElementsByClassName('project-info');
     var links = document.getElementsByClassName('project-link');
-    var maininfo = document.getElementsByClassName('main-info');
-    var contacts = document.getElementsByClassName('contacts');
 
     var animate = function(e) {
             x = ((e.clientX/window.innerWidth)*2-1)*45;
@@ -40,21 +38,26 @@ window.onload = function() {
             }
     }
 
-    var borderTouch = window.innerWidth*0.25;
-    var margkTouch = 1;
-    var leftSide = window.innerWidth*0.2;
-
-    var touchanimate2 = function(e) {
+    borderTouch = window.innerWidth*0.3;
+    var borderRate = window.innerWidth/335;
+    if (borderTouch < 335) {
+        if (borderRate < 2) {
+            borderTouch = window.innerWidth;
+        } else {
+            borderTouch = 335;
+        }
+    }
+    var touchanimate = function(e) {  
         var touch = e.touches[0];
         x = ((touch.clientX/window.innerWidth)*2-1)*45;
         y = ((touch.clientY/window.innerHeight)*2-1)*45;
-        onetozero = Math.abs((touch.clientX/(window.innerWidth-border))-1);
+        onetozero = Math.abs((touch.clientX/(window.innerWidth-borderTouch))-1);
         margin = touch.clientX*margk;
         margintop = Math.abs((touch.clientY/(window.innerHeight))-1)*margtopk;
         scale = onetozero;
         rotate = onetozero*45;
         wrap.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
-        if (touch.clientX < window.innerWidth-border) {
+        if (touch.clientX < window.innerWidth-borderTouch) {
             for (i=0; i<projects.length; i++) {
                 projects[i].style.transform = 'scale(' + (scale/i + 1) + ')';
             }
@@ -72,57 +75,26 @@ window.onload = function() {
                 links[i].style.transform = 'rotate(' + 0 + 'deg)';
                 links[i].style.width = '100%';
             }
-            wrap.style.marginLeft = (window.innerWidth-border)*margk + 'px';
+            wrap.style.marginLeft = (window.innerWidth-borderTouch)*margk + 'px';
         }
-}
-
-    var touchanimate = function(e) {
-        var touch = e.touches[0];
-        x = ((touch.clientX/window.innerWidth)*2-1)*45;
-        y = ((touch.clientY/window.innerHeight)*2-1)*45;
-        zerotoone = (touch.clientX-leftSide)/(window.innerWidth-borderTouch-leftSide);
-        margin = (touch.clientX-leftSide)*margkTouch;
-        margintop = touch.clientY/(window.innerHeight)*margtopk;
-        scale = zerotoone;
-        rotate = zerotoone*45;
-        for (i=0; i<links.length; i++) {
-            links[i].style.marginTop =  margintop + 'px';
-        }
-        //wrap.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
-        if (touch.clientX <= leftSide) {
-            for (i=0; i<projects.length; i++) {
-                projects[i].style.transform = 'scale(' + 1 + ')';
-            }
-            for (i=0; i<links.length; i++) {
-                links[i].style.transform = 'rotate(' + 0 + 'deg)';
-                links[i].style.width = '100%';
-            }
-            wrap.style.marginLeft = 0 + 'px';
-        }
-        if ((leftSide < touch.clientX) && (touch.clientX < window.innerWidth-borderTouch)) {
-            for (i=0; i<projects.length; i++) {
-                projects[i].style.transform = 'scale(' + (scale/i + 1) + ')';
-                projects[i].style.width = window.innerWidth - margin-48 + 'px';
-            }
-            for (i=0; i<links.length; i++) {
-                links[i].style.transform = 'rotate(' + (rotate/i) + 'deg)';
-                links[i].style.width = 'auto';
-                links[i].style.width = window.innerWidth - margin-48 + 'px';
-            }
-            wrap.style.marginLeft = margin + 'px';
-        }
-        if (touch.clientX > window.innerWidth-borderTouch) {
-            wrap.style.marginLeft = (window.innerWidth-borderTouch-leftSide)*margkTouch + 'px';
-        }
-        
 }
 
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (isMobile || (window.innerWidth < 1080)){
-        //wrap.classList.add('break');
-        // document.documentElement.style.overflowY = 'hidden';
-        window.addEventListener('touchmove',touchanimate2);
+        if (borderTouch == window.innerWidth){
+
+        } else {
+            wrap.style.transform = 'rotateX(-' + 45 + 'deg) rotateY(' + 20 + 'deg)';
+            for (i=0; i<projects.length; i++) {
+                projects[i].style.transform = 'scale(' + (1/i + 1) + ')';
+            }
+            for (i=0; i<links.length; i++) {
+                links[i].style.transform = 'rotate(' + (45/i) + 'deg)';
+            }
+        }
+        window.addEventListener('touchmove',touchanimate);
     } else {
+        wrap.style.opacity = '0';
         window.addEventListener('mousemove',animate);
         document.addEventListener('mouseleave',function(){
             wrap.style.opacity = '0.1';
